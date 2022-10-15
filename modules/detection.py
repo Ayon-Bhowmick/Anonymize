90f530f6419f85a036e5473ab00549fed2064bfb
Ayon Bhowmick <ayb224@lehigh.edu>
2022-10-15 10:58:36 -0400

    fixes

--- modules/detection.py
+++ modules/detection.py
@@ -1,6 +1,6 @@
 import cv2
 
-def face(cascade, frame, x, y, w, h):
+def face(cascade, frame, x, y, w, h) -> tuple[int]:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     face = cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
     if len(face) != 0:
@@ -9,9 +9,9 @@ def face(cascade, frame, x, y, w, h):
                 if abs(fin[i] - face[0][i]) > 20:
                     fin[i] = face[0][i]
             x, y, w, h = fin
-    return x, y, w, h
+    return (x, y, w, h)
 
-def eyes(cascade, frame, x, y, w, h):
+def eyes(cascade, frame, x, y, w, h) -> tuple[int]:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     eyes = cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
     if len(eyes) == 2:
@@ -25,4 +25,4 @@ def eyes(cascade, frame, x, y, w, h):
             if abs(fin[i] - temp[i]) > 20:
                 fin[i] = temp[i]
         x, y, w, h = fin
-    return x, y, w, h
+    return (x, y, w, h)
--- modules/filters.py
+++ modules/filters.py
@@ -1,6 +1,7 @@
 import cv2
+import numpy
 
-def gaussian(frame, x, y, w, h):
+def gaussian(frame, x, y, w, h) -> numpy.ndarray:
     W = w // 3
     H = h // 3
     if W % 2 == 0:
@@ -10,7 +11,7 @@ def gaussian(frame, x, y, w, h):
     frame[y:y+h, x:x+w] = cv2.GaussianBlur(frame[y:y+h, x:x+w], (W, H), 30)
     return frame
 
-def pixelate(frame, x, y, w, h):
+def pixelate(frame, x, y, w, h) -> numpy.ndarray:
     pixelated = cv2.resize(frame[y:y+h, x:x+w], (w//10, h//10), interpolation=cv2.INTER_NEAREST)
     pixelated = cv2.resize(pixelated, (w, h), interpolation=cv2.INTER_NEAREST)
     frame[y:y+h, x:x+w] = pixelated
